22b66e08190a849e857a1d6d57e061762748add5
NormanBl <norman.blaeker@gmail.com>
2023-10-03 20:35:10 +0200

    add getmapping by id

--- src/main/java/de/norman/webshop/controller/ProductController.java
+++ src/main/java/de/norman/webshop/controller/ProductController.java
@@ -4,6 +4,7 @@ import de.norman.webshop.model.*;
 import de.norman.webshop.repository.ProductResponseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +16,11 @@ public class ProductController {
     @GetMapping("/products")
     public Iterable<ProductResponse> getAllProducts() {
         return productResponseRepository.findAll();
+    }
+
+    @GetMapping("/products/id")
+    public Iterable<ProductResponse> getAllProducts(@RequestParam long id) {
+        return productResponseRepository.findAllById(id);
     }
 
 }
